1af0e52764628cc48e472c14cae2646a366b2e56
joaodvalexandre <106627264+joaodvalexandre@users.noreply.github.com>
2022-05-31 23:32:46 +0100

    Update on the Index
    Created new pages for each portfolio;
    Updates on the index part;
    Improved links to pages.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-// Modal Image Gallery
-function onClick(element) {
-  document.getElementById("img01").src = element.src;
-  document.getElementById("modal01").style.display = "block";
-  var captionText = document.getElementById("caption");
-  captionText.innerHTML = element.alt;
-}
-
 // Change style of navbar on scroll
 window.onscroll = function() {myFunction()};
 function myFunction() {
